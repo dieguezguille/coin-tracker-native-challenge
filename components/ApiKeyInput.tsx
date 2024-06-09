512ff88f76527b9f302df6ee922ff9ee52576794
Guillermo Agustín Diéguez <guillermodieguez@outlook.com>
2024-06-09 15:03:09 -0300

    ✨ restore persisted value into input

--- components/ApiKeyInput.tsx
+++ components/ApiKeyInput.tsx
@@ -1,9 +1,11 @@
 import { setApiKeyValue } from "@/features/coin-gecko/coinGeckoSlice";
-import React, { useState } from "react";
+import { RootState } from "@/store/store";
+import React, { useEffect, useState } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const ApiKeyInput: React.FC = () => {
+  const { apiKeyValue } = useSelector((state: RootState) => state.coinGecko);
   const dispatch = useDispatch();
 
   const [input, setInput] = useState<string>("");
@@ -12,18 +14,24 @@ const ApiKeyInput: React.FC = () => {
     dispatch(setApiKeyValue(input));
   };
 
+  useEffect(() => {
+    if (apiKeyValue) {
+      setInput(apiKeyValue);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
         focusable
-        placeholder="Enter CoinGecko API Key"
+        placeholder="Enter your CoinGecko API Key"
         value={input}
         onChangeText={setInput}
       />
 
       <View style={styles.actionButton}>
-        <Button color={"#fff"} title="Save API Key" onPress={saveApiKey} />
+        <Button color="#ffffff" title="Save" onPress={saveApiKey} />
       </View>
     </View>
   );
@@ -36,6 +44,7 @@ const styles = StyleSheet.create({
   },
   input: {
     borderColor: "#ccc",
+    fontSize: 16,
     borderWidth: 1,
     borderRadius: 5,
     padding: 10,
